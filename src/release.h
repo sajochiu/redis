974bdb6003d0d23432d395de5d53f77be164c049
Joseph <sajochiu@gmail.com>
2021-06-24 15:45:40 +0800

    QQ

--- src/release.h
+++ src/release.h
@@ -1,3 +1,3 @@
-#define REDIS_GIT_SHA1 "8d2ba50b"
-#define REDIS_GIT_DIRTY "11"
-#define REDIS_BUILD_ID "computer-1624517745"
+#define REDIS_GIT_SHA1 "b6e87af3"
+#define REDIS_GIT_DIRTY "0"
+#define REDIS_BUILD_ID "computer-1624520539"
